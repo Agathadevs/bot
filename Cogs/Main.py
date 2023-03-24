8d85043df37ea5d519a673baba85db5d09bc0327
Chenchi2907 <105590093+Chenchi2907@users.noreply.github.com>
2023-03-24 20:31:24 +0800

    Create Main.py

---
+++ Cogs/Main.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+
+class Main(commands.Cog):
+    def __init__(self,bot):
+        self.bot=bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        await self.bot.change_presence(activity=discord.Game('im online'))
+        print('is online')
+    @commands.command()
+    async def ping(self,ctx):
+        await ctx.send(f"{round(self.bot.latency*1000)} (ms)")
+    @commands.Cog.listener()
+    async def on_message(self,msg):
+        if msg.content=="嗨":
+            await msg.channel.send("嗨OwO")
+    
+        if msg.content=="?":
+            await msg.channel.send("蝦?")
+    
+        if msg.content=="沒事":
+            await msg.channel.send("好窩owo")
+def setup(bot):
+    bot.add_cog(Main(bot))
