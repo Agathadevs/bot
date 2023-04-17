7351241d6202e2f61762fb807a2a5e064f69fd48
Chenchi2907 <105590093+Chenchi2907@users.noreply.github.com>
2023-04-17 21:46:04 +0800

    Update playmusic.py

--- src/music/playmusic.py
+++ src/music/playmusic.py
@@ -1,9 +1,9 @@
 import discord
 from discord.ext import commands
-import yt_dlp
 from ebs import lib
 from ebs import embed
-import time
+import yt_dlp
+import asyncio
 class Main_third(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -15,6 +15,7 @@ class Main_third(commands.Cog):
         self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
         self.vc = None
 
+     #searching the item on youtube
     def search_yt(self,item):
         with yt_dlp.YoutubeDL(self.YDL_OPTIONS) as ydl:
             try: 
@@ -28,28 +29,30 @@ class Main_third(commands.Cog):
                     "youtube_url":info['webpage_url'],
                     "duration":info["duration"]
                    }
-    
-    async def play_next(self,ctx,url,song_len):
-            if len(self.music_queue) > 0:
-                self.is_playing = True
 
-                #get the first url
-                m_url = self.music_queue[0][0]['source']
+    async def play_next(self,ctx):
+        if len(self.music_queue) > 0:
+            self.is_playing = True
+            song_len=len(self.music_queue)
+            url=self.music_queue[0][0]["youtube_url"]
+            msg=embed.music_embed(ctx,url,song_len)
+            emb=msg['Emb']
+            await ctx.send(embed=emb)
 
-                
-                self.music_queue.pop(0)
-                msg=embed.music_embed(ctx,url,song_len)
-                emb=msg['Emb']
-                await ctx.send(embed=emb)
-                try:
-                    await self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e:  self.play_next(ctx,url,song_len))
-                except Exception as error:
-                    print(f"發生錯誤  {error}")
-            else:
-                self.is_playing = False
+            #get the first url
+            m_url = self.music_queue[0][0]['source']
 
+            #remove the first element as you are currently playing it
+            self.music_queue.pop(0)
+             
+            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e:  asyncio.ensure_future(self.play_next(ctx)))
+            
+            
+        else:
+            self.is_playing = False
+        
     # infinite loop checking 
-    async def play_music(self,ctx,url,song_len):
+    async def play_music(self,ctx):
         if len(self.music_queue) > 0:
             self.is_playing = True
 
@@ -67,14 +70,25 @@ class Main_third(commands.Cog):
             
             print(self.music_queue)
             #remove the first element as you are currently playing it
+            song_len=len(self.music_queue)
+            url=self.music_queue[0][0]["youtube_url"]
+            msg=embed.music_embed(ctx,url,song_len)
+            emb=msg['Emb']
+            await ctx.send(embed=emb)
+
+            async with ctx.typing():
+                await asyncio.sleep(1)
+
+            if self.vc.is_playing():
+                 self.vc.stop()
+
+            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: asyncio.run_coroutine_threadsafe(self.play_next(ctx), self.bot.loop).result())
             self.music_queue.pop(0)
-            try:
-                self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next(ctx,url,song_len))
-            except Exception as error:
-                print(f" 發生錯誤  {error}")
         else:
+
+            
             self.is_playing = False
-    
+
     @commands.command(name="play", help="Plays a selected song from youtube")
     async def p(self, ctx, *args):
         query = " ".join(args)
@@ -82,21 +96,17 @@ class Main_third(commands.Cog):
         voice_channel = ctx.author.voice.channel
         if voice_channel is None:
             #you need to be connected so that the bot knows where to go
-            await ctx.send("☑️| 成功連結至頻道")
+            await ctx.send("Connect to a voice channel!")
         else:
             song = self.search_yt(query)
             if type(song) == type(True):
                 await ctx.send("Could not download the song. Incorrect format try another keyword. This could be due to playlist or a livestream format.")
             else:
-                await ctx.send("歌曲已加入撥放清單")
+                await ctx.send("Song added to the queue")
                 self.music_queue.append([song, voice_channel])
-                song_len=len(self.music_queue)
+                
                 if self.is_playing == False:
-                    await self.play_music(ctx,song['youtube_url'],song_len)
-                    
-                    msg=embed.music_embed(ctx,song['youtube_url'],song_len)
-                    emb=msg['Emb']
-                    await ctx.send(embed=emb)
+                    await self.play_music(ctx)
 
     @commands.command(name="queue", help="Displays the current songs in queue")
     async def q(self, ctx):
@@ -116,12 +126,12 @@ class Main_third(commands.Cog):
             self.vc.stop()
             #try to play next in the queue if it exists
             await self.play_music()
-                
-
+            
+    @commands.command(name="disconnect", help="Disconnecting bot from VC")
+    async def dc(self, ctx):
+        await self.vc.disconnect()
 async def setup(bot):
-    await bot.add_cog(Main_third(bot))
-   
-   
+    await bot.add_cog(Main_third(bot))  
    
    
     
