af481be307379d25ac1972875cd4167c08b2c730
Cheng <105590093+Cheng0116@users.noreply.github.com>
2023-08-06 19:21:28 +0800

    Update buttontest.py
    Signed-off-by: Cheng <105590093+Cheng0116@users.noreply.github.com>

--- src/buttontest.py
+++ src/buttontest.py
@@ -4,24 +4,24 @@ from discord import app_commands
 
 
 class ButtonHandler(discord.ui.View):
-    @discord.ui.button(label="A",
+    @discord.ui.button(label="",
                        style=discord.ButtonStyle.primary)
     async def buttonA(self,
                       interaction:discord.Interaction,
                       button:discord.ui.Button):
-        await interaction.response.edit_message(content="你選擇了A")
-    @discord.ui.button(label="B",
+        await interaction.response.edit_message(content="")
+    @discord.ui.button(label="",
                        style=discord.ButtonStyle.primary)
     async def buttonB(self,
                       interaction:discord.Interaction,
                       button:discord.ui.Button):
-        await interaction.response.edit_message(content="你選擇了B")
+        await interaction.response.edit_message(content="")
 class quiz(commands.Cog):
     def __init__(self, bot : commands.Bot):
         self.bot=bot
-    @app_commands.command(name="quiz",description="start a quiz")
+    @app_commands.command(name="quiz",description="")
     async def quiz(self , interaction:discord.Interaction):
         view=ButtonHandler()
-        await interaction.response.send_message(f"開始考試",view=view)
+        await interaction.response.send_message(f"",view=view)
 async def setup(bot:commands.Bot):
     await bot.add_cog(quiz(bot))
